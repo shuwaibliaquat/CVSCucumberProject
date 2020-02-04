f448d6819c4f48dfa43e1a559784c9ca2fe7cc59
JavaLearner <shuwaibliaquat@hotmail.comgit config --global user.name shuwaibliaquat>
2020-02-04 03:55:57 -0500

    TestNG xml anAbstractTestNGCucumberTests extends in test runner

--- src/test/java/com/qa/runner/TestRunner.java
+++ src/test/java/com/qa/runner/TestRunner.java
@@ -2,6 +2,7 @@ package com.qa.runner;
 
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
@@ -22,6 +23,6 @@ import cucumber.api.CucumberOptions;
   
 
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests  {//AbstractTestNGCucumberTests-comes from testNGCucumber Dependency for TestNG.xml execution for maven test using jenkins or control panel
 
 }
